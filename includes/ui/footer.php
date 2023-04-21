7141626849cd6e40441660ad4ab35918336f42e0
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2023-04-21 10:21:17 +0600

    cdn js added

--- includes/ui/footer.php
+++ includes/ui/footer.php
@@ -10,5 +10,11 @@
         </footer>
     </div>
     <!-- /.container -->
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.4.slim.min.js" integrity="sha256-a2yjHM4jnF9f54xUQakjZGaqYs/V1CYvWpoqZzC2/Bw=" crossorigin="anonymous"></script>
+    <!-- Custom js -->
+    <script src="js/script.js"></script>
 </body>
 </html>
